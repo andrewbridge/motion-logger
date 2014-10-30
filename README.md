bd1c0a41694684e5abb6263d6db9960d55f5797e
Andrew Bridge <me@andrewhbridge.co.uk>
2014-10-30 02:45:44 +0000

    Improving flexibility of collector API, adding in connection with mongodb.

--- README.md
+++ README.md
@@ -6,3 +6,5 @@ Accelerometer-based side-channel attacks research project
 Requirements
 ------------
 The projects is currently written in Jade, LESS, CoffeeScript and Javascript (for the backend). For this reason, there is a requirement for Node.js to be used for the backend APIs. Once compiled, the frontend files may be used with a basic static server.
+
+A mongodb database is also used on the backend for data storage.
--- collector-api/api.js
+++ collector-api/api.js
@@ -1,50 +1,74 @@
 #!/usr/bin/env node
 
-var restify = require('restify')
-  , subjSave = require('save')('testSubjects')
-  , chance = require('chance').Chance()
-  , crypto = require('crypto');
+var restify = require('restify') //API server
+  , save = require('save') //Database interface
+  , saveMongodb = require('save-mongodb')
+  , chance = require('chance').Chance() //Salt generator
+  , crypto = require('crypto') //Hasher
+  , Db = require('mongodb').Db //MongoDB Database object
+  , DbServer = require('mongodb').Server //MondoDB Server object
+  , subjSaveDb = new Db('TestSubjects', new DbServer('localhost', 27017, {})) //Initialisation of the TestSubjects database
+  , subjSave; //Initialising db interface object
 
-var server = restify.createServer({ name: 'motion-collector-api'})
+if (process.argv[2] == "--dryrun") {
+	console.log("Running in dry run mode, none of the data passed through the API will be saved!");
+	subjSave = save('TestSubjects');
+	init();
+} else {
+	console.log("Running in normal mode, all data will be saved to the specified DB!\nOpening database...");
+	//Open database
+	subjSaveDb.open(function(error, connection) {
+		if (error) {console.log("Error opening database! "+JSON,stringify(error.errors));}
 
-server.listen(8080, function() {
-	console.log('%s listening at %s', server.name, server.url)
-})
-
-server
-	.use(restify.fullResponse())
-	.use(restify.bodyParser())
-
-server.get('/newsession', function(req, res, next) {
-	subjSave.create({ subjectCreation: new Date().getTime() }, function(saveErr, savedObject) {
-		if (saveErr) return next(new restify.InvalidArgumentError(JSON.stringify(saveErr.errors)));
-
-		var shasum = crypto.createHash('sha1');
-		shasum.update(savedObject._id+chance.string({length: 20}), 'utf-8');
-		subjSave.update({ _id: savedObject._id, hash: shasum.digest('hex')}, function(updateErr, updatedObject) {
-			if (updateErr) return next(new restify.InvalidArgumentError(JSON.stringify(updateErr.errors)));
-				console.log(updatedObject);
-
-			res.send(201, {id: updatedObject.hash});
+		connection.collection('sessions', function(error, collection) {
+			console.log("Chosen collection, successful connection...");
+			subjSave = save('TestSessions', {engine: saveMongodb(collection)});
+			init();
 		});
 	});
-});
+}
 
-server.put('/session/:hash', function(req, res, next) {
-	var dataArr;
-	try {
-		dataArr = JSON.parse(req.params.data);
-	} catch(e) {
-		return next(new restify.InvalidArgumentError('The data provided was corrupted.'));
-	}
-	subjSave.findOne({hash: req.params.hash}, function(findErr, subj) {
-		if (findErr) return next(new restify.InvalidArgumentError(JSON.stringify(findErr.errors)));
+function init() {
+	var server = restify.createServer({ name: 'motion-collector-api'});
 
-		subjSave.update({_id: subj._id, data: (subj.data||[]).concat(dataArr)}, function(updateErr, updatedObject) {
-			if (updateErr) return next(new restify.InvalidArgumentError(JSON.stringify(updateErr.errors)));
+	server.listen(8080, function() {
+		console.log('%s listening at %s', server.name, server.url)
+	})
 
-			res.send();
+	server
+		.use(restify.fullResponse())
+		.use(restify.bodyParser())
+
+	server.get('/newsession', function(req, res, next) {
+		subjSave.create({ subjectCreation: new Date().getTime() }, function(saveErr, savedObject) {
+			if (saveErr) return next(new restify.InvalidArgumentError(JSON.stringify(saveErr.errors)));
+
+			var shasum = crypto.createHash('sha1');
+			shasum.update(savedObject._id+chance.string({length: 20}), 'utf-8');
+			subjSave.update({ _id: savedObject._id, hash: shasum.digest('hex')}, function(updateErr, updatedObject) {
+				if (updateErr) return next(new restify.InvalidArgumentError(JSON.stringify(updateErr.errors)));
+					console.log(updatedObject);
+
+				res.send(201, {id: updatedObject.hash});
+			});
 		});
 	});
-	subjSave.update({hash: req.params.hash, data: data})
-});
+
+	server.put('/session/:hash', function(req, res, next) {
+		var dataArr;
+		try {
+			dataArr = JSON.parse(req.params.data);
+		} catch(e) {
+			return next(new restify.InvalidArgumentError('The data provided was corrupted.'));
+		}
+		subjSave.findOne({hash: req.params.hash}, function(findErr, subj) {
+			if (findErr) return next(new restify.InvalidArgumentError(JSON.stringify(findErr.errors)));
+
+			subjSave.update({_id: subj._id, data: (subj.data||[]).concat(dataArr)}, function(updateErr, updatedObject) {
+				if (updateErr) return next(new restify.InvalidArgumentError(JSON.stringify(updateErr.errors)));
+
+				res.send();
+			});
+		});
+	});
+}
