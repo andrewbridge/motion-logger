5c98e02a04b8e96df871f7481b0500512f5e1fc1
Andrew Bridge <me@andrewhbridge.co.uk>
2015-02-24 01:18:04 +0000

    Backing up mongodb test data db, making multiple fixes to keypress learner, adding running commentary and producing detection data json file.

--- keypress-detector/learner.js
+++ keypress-detector/learner.js
@@ -7,6 +7,7 @@
 //Shim for Promises
 if (typeof Promise == "undefined") {Promise = require("promise");}
 
+var fs = require('fs'); // FileSystem
 var save = require('save'); //Database interface
 var saveMongodb = require('save-mongodb');
 var Db = require('mongodb').Db; //MongoDB Database object
@@ -29,15 +30,11 @@ var networks = {oBeta: {
 var downUpDiff = []; // Min and Max difference between keydown and keyup events.
 var promExtractor = function(resolve, reject) {this.resolve = resolve; this.reject = reject;};
 
-var subjSaveDb = new this.Db('TestSubjects', new this.DbServer('localhost', 27017, {})) //Initialisation of the TestSubjects database
+var subjSaveDb = new Db('TestSubjects', new DbServer('localhost', 27017, {})) //Initialisation of the TestSubjects database
     , subjCol; //Initialising db interface object
-var resuSaveDb = new this.Db('keyPressSystem', new this.DbServer('localhost', 27017, {})) //Initialisation of the keyPressSystem database
-    , resuCol; //Initialising db interface object
 
 var testProm = {};
 var testDone = new Promise(promExtractor.bind(testProm));
-var resuProm = {};
-var resuDone = new Promise(promExtractor.bind(resuProm));
 
 subjSaveDb.open(function (error, connection) {
     if (error) {
@@ -56,24 +53,7 @@ subjSaveDb.open(function (error, connection) {
     });
 });
 
-resuSaveDb.open(function (error, connection) {
-    if (error) {
-        resuProm.reject("Error opening keyPressSystem database! " + JSON.stringify(error.errors));
-        return false;
-    }
-
-    connection.collection('data', function (error, collection) {
-        if (error) {
-            resuProm.reject("Error choosing keyPressSystem data collection! " + JSON.stringify(error.errors));
-            return false;
-        }
-        console.log("Chosen keyPressSystem data collection, successful connection...");
-        resuCol = save('keyPressData', {engine: saveMongodb(collection)});
-        resuProm.resolve();
-    });
-});
-
-Promise.all([testDone, resuDone]).then(finishSetup, function() {
+Promise.all([testDone]).then(finishSetup, function() {
     console.log("ERROR: An error occurred opening the databases.");
     throw new Error("An error occurred opening the databases.");
 });
@@ -82,10 +62,12 @@ function finishSetup() {
     subjCol.count({}, function(cnterr, data) {
         if (cnterr) {return new Error("There was an error counting the data items.");}
         if (data > currentRecord) {
-            subjCol.find().skip(currentRecord).limit(1, function(findErr, data) {
+            subjCol.find({}, function(findErr, data) {
                 if (findErr) {return new Error("There was an error choosing a data item.");}
                 try {
-                    var finalData = JSON.parse(data.data);
+                    console.log("Attempting to select record for learning. Trying record: "+currentRecord);
+                    var finalData = data[currentRecord].data;
+                    console.log("Selection and parsing successful. Sending for prep.");
                     currentRecord++;
                     learn(finalData);
                 } catch (e) {
@@ -93,28 +75,25 @@ function finishSetup() {
                 }
             });
         } else {
-            saveValToDb("downUpRange", JSON.stringify(downUpDiff));
-            saveValToDb("oBetaPrePress", networks.oBeta.pre.toJSON());
-            saveValToDb("oBetaPostPress", networks.oBeta.post.toJSON());
-            saveValToDb("oGammaPrePress", networks.oGamma.pre.toJSON());
-            saveValToDb("oGammaPostPress", networks.oGamma.post.toJSON());
-            saveValToDb("aZPrePress", networks.aZ.pre.toJSON());
-            saveValToDb("aZPostPress", networks.aZ.post.toJSON());
-            saveValToDb("aYPrePress", networks.aY.pre.toJSON());
-            saveValToDb("aYPostPress", networks.aY.post.toJSON());
-        }
-    });
-}
-
-function saveValToDb(key, data) {
-    resuCol.findOne({"key": key}, function(findErr, foundObj) {
-        if (findErr) {
-            resuCol.create({"key": key, "data": data}, function(createErr, newObj) {
-                if (createErr) {return new Error("There was an error creating the "+key+" value in the db.");}
-            });
-        } else {
-            resuCol.update({"key": key, "data": data}, function(updateErr, updatedObj) {
-                if (updateErr) {return new Error("There was an error updating the "+key+" value in the db.");}
+            var dataObj = {
+                downUpRange: JSON.stringify(downUpDiff),
+                oBetaPrePress: networks.oBeta.pre.toJSON(),
+                oBetaPostPress: networks.oBeta.post.toJSON(),
+                oGammaPrePress: networks.oGamma.pre.toJSON(),
+                oGammaPostPress: networks.oGamma.post.toJSON(),
+                aZPrePress: networks.aZ.pre.toJSON(),
+                aZPostPress: networks.aZ.post.toJSON(),
+                aYPrePress: networks.aY.pre.toJSON(),
+                aYPostPress: networks.aY.post.toJSON()
+            };
+            fs.writeFile("./keypress_detection_data.json", JSON.stringify(dataObj), function(err) {
+                if (err) {
+                    console.log("An issue occurred saving the detection data.");
+                } else {
+                    console.log("Detection data saved.");
+                }
+                console.log("Finished learning!");
+                process.exit(0);
             });
         }
     });
@@ -151,9 +130,12 @@ function arrToBrainFormat(outputVal, val) {
 }
 
 function learn(dataArr) {
-    var stream = require("./clouded-sky.js")(dataArr);
+    console.log("Beginning learning...");
+    var stream = require("./clouded-sky.js").init(dataArr);
     var datapoint;
     var exit = false;
+    var hasPress = false;
+    var firstResponseThreshold = 1250; //Wait a second and a quarter before assuming a dud dataset.
     var trailLen = 4;
     var trailCountDown;
     var trailAcceptanceThreshold = 0.001; // The smallest range that should be exhibited in a trail for it to be included.
@@ -169,13 +151,22 @@ function learn(dataArr) {
     // prePress - Contains arrays of [trailLen] pings before each keydown (keydown is included too, making [trailLen+1] values)
     // postPress - Contains arrays of [trailLen] pings after each keyup (keyup is included too, making [trailLen+1] values)
     var downTime;
-    while(datapoint = stream.pick(0) && !exit) {
+    var count = 0;
+    var strmLen = stream.store.length;
+    while((datapoint = stream.pick(0)[0]) && !exit) {
+        count++;
+        if (Boolean(process.stdout.isTTY)) {
+            process.stdout.write("Prepping datapoint "+count+" of "+strmLen+": Type: "+datapoint.data.event);
+        } else {
+            console.log("Prepping datapoint "+count+" of "+strmLen+": Type: "+datapoint.data.event);
+        }
         switch(datapoint.data.event) {
             case "ping":
-                if (!datapoint.data.datapoints.orientation.x || !datapoint.data.datapoints.orientation.y
-                    || !datapoint.data.datapoints.acceleration.z || !datapoint.data.datapoints.acceleration.y) {
+                if (datapoint.time > firstResponseThreshold && (!datapoint.data.datapoints.orientation.x || !datapoint.data.datapoints.orientation.y
+                    || !datapoint.data.datapoints.acceleration.z || !datapoint.data.datapoints.acceleration.y)) {
                     exit = true;
-                } else {
+                } else if ((datapoint.data.datapoints.orientation.x && datapoint.data.datapoints.orientation.y
+                    && datapoint.data.datapoints.acceleration.z && datapoint.data.datapoints.acceleration.y)) {
                     oBeta.pings.push(datapoint.data.datapoints.orientation.x);
                     oGamma.pings.push(datapoint.data.datapoints.orientation.y);
                     aZ.pings.push(datapoint.data.datapoints.acceleration.z);
@@ -200,6 +191,7 @@ function learn(dataArr) {
                 break;
             case "keydown":
                 if (oBeta.pings.length >= 6) {
+                    hasPress = true;
                     //Only add data if it has a change above the trail acceptance threshold.
                     var oBetaTrail = normaliseArrData(oBeta.pings.slice(oBeta.pings.length-trailLen, oBeta.pings.length).concat(datapoint.data.datapoints.orientation.x), ranges.oBeta);
                     if (findRange(oBetaTrail) != trailAcceptanceThreshold) {oBeta.prePress.push(oBetaTrail);}
@@ -224,9 +216,17 @@ function learn(dataArr) {
                 }
                 break;
         }
+        if (!stream.isEmpty()) {
+            process.stdout.clearLine();  // clear current text
+            process.stdout.cursorTo(0);  // return cursor
+        } else {
+            process.stdout.write("\n");
+        }
     }
-    if (!exit) {
-        var data;
+    if (!exit && hasPress) {
+        console.log("Prep complete!");
+        var data = {oBeta: {}, oGamma: {}, aZ: {}, aY: {}};
+        console.log("Formatting");
         data.oBeta.prePress = oBeta.prePress.map(arrToBrainFormat.bind(this, {keypress: 1}));
         data.oBeta.postPress = oBeta.postPress.map(arrToBrainFormat.bind(this, {keypress: 1}));
         data.oGamma.prePress = oGamma.prePress.map(arrToBrainFormat.bind(this, {keypress: 1}));
